2a62b1e06ad73df6967c7b64a77fa63336f8b8ab
Heidi <emhp009@gmail.com>
2024-04-20 18:43:51 +0200

    about us started

---
+++ components/AboutUs.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AboutUs = () => {
+  return 
+  <div>
+    <div className=" bg-gray-300">
+      <h1>The Historical Committe</h1>
+      <p>KSSS has since 2.5 years back formed a Historical Committee (HC) with the task of mapping what we have, for example books, photos, films,  prizes, trophies, models, etc. </p>
+      </div>
+
+      <section>
+        <p>KSSS (The Royal Yacht Club) is one of the most oldest and largest yacht clubs in the world. It was founded un 1830 and currently has close to 6,000 members, out of which about 2,500 are younger than 20 years old. KSSS also has a very broad base of ativities areound sport of sailing; from youth camps, training classes, regattas (from smaller local races, to the world largest international offshore race "Gotand Runt"), to Olympic ambitions (eg 2 silver in Tokyo, 2021)</p>
+      </section>
+
+<div className="">
+
+</div>
+
+  </div>
+};
+
+export default AboutUs;
