e06c36dff2510d2cf846caa4d7a13cde9f9b7ada
Heidi <emhp009@gmail.com>
2024-04-24 13:30:16 +0200

    classname changed

--- app/landing-page/landingPage.jsx
+++ app/landing-page/landingPage.jsx
@@ -12,25 +12,25 @@ export default async function LandingPage() {
     <div>
 
       <section className="achievement">
-        <div><p className="achievement-number">200+</p>
-        <p className="achievement-text">år av historia</p></div>
-        <div><p className="achievement-number">60+</p>
-        <p className="achievement-text">årsbocker</p></div>
-        <div><p className="achievement-number">50+</p>
-        <p className="achievement-text"> medaljer</p></div>
+        <div><p className="achievement__number">200+</p>
+        <p className="achievement__text">år av historia</p></div>
+        <div><p className="achievement__number">60+</p>
+        <p className="achievement__text">årsbocker</p></div>
+        <div><p className="achievement__number">50+</p>
+        <p className="achievement__text"> medaljer</p></div>
       </section>
       
       <section>    
         <Image src={rectangle} alt="KSSS historia" className="ksss-historia" />
         <div className="updates">
-        <h2 className="updates-header">Från startskott till idag</h2>
-        <p className="updates-text">Utforska KSSS historia, från dess grundande 1830, och upptäck en rik värld av seglingskultur och framstående prestationer. Det finns mycket att hitta!</p>
+        <h2 className="updates__header">Från startskott till idag</h2>
+        <p className="updates__text">Utforska KSSS historia, från dess grundande 1830, och upptäck en rik värld av seglingskultur och framstående prestationer. Det finns mycket att hitta!</p>
       </div>
 </section>
 
-<section className="event-section">
-  <h3 className="event-header">Milstolpar i vår historia</h3>
-  <div className="event-card">
+<section className="event__section">
+  <h3 className="event__header">Milstolpar i vår historia</h3>
+  <div className="event__card">
     <h3>1905</h3> 
 <p>Svenska Seglarförbundet bildas </p>
   </div>
@@ -38,9 +38,9 @@ export default async function LandingPage() {
 </section>
 
 
-<section className="omoss-box">
-  <h6 className="omoss-header">träffa historiska kommitteen</h6>
-  <p className="omoss-text">Träffa personerna som arbetar på att bevara KSSS historia</p>
+<section className="omoss__box">
+  <h6 className="omoss__header">träffa historiska kommitteen</h6>
+  <p className="omoss__text">Träffa personerna som arbetar på att bevara KSSS historia</p>
   <BtnLight>OM OSS</BtnLight>
 </section>
       
--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import Image from 'next/image';
-import coke from '../../public/images/coke.webp';
-import hh from '../../public/images/hh.jpeg';
-import stockholm from '../../public/images/stockholm.webp';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <div>
-      <div className='footer-container'>
-        <div className='kontak-container'>
-          <h4 className='kontak-text'>KONTAKT</h4>
-          <div className='email-info'>
+      <div className='footer__container'>
+        <div className='kontak__container'>
+          <h4 className='kontak__text'>KONTAKT</h4>
+          <div className='email__info'>
             <p className='text-sm font-semibold'>EMAIL</p> 
             <p className='text-sm underline'>kssshistory@ksss.se</p>
           </div>
-          <div className='number-info'>
-            <p className='font-semibold text-sm'>TELEFON</p> 
-            <p className='text-sm'>+46(0)045526366</p>
+          <div className='number__info'>
+            <p className='footer__telefon'>TELEFON</p> 
+            <p className='footer__telefon__nummer'>+46(0)045526366</p>
           </div>
         </div>
-        <div className='website-container'>
+        <div className='website__container'>
           <h4>WEBSITE</h4>
           <ul>
             <li>Om oss</li>
