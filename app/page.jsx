3812e106272692eadde7a20e24df31e794f4d7dd
Heidi <emhp009@gmail.com>
2024-04-29 16:44:11 +0200

    up

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import { getTimeline } from "../sanity/sanity-utils"
 import AboutUs from "./about-us/AboutUs";
 import LandingPage from "./landing-page/landingPage";
+import Link from "next/link";
 
 export default async function Home() {
   const timeline = await getTimeline();
